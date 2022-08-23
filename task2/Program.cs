f04867822975fb183a1ad52c3851014bd0bcfb02
tati <ost11tan@gmail.com>
2022-08-23 05:29:37 +0300

    task2,на заданных в тз значениях работает

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.Write("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число:");
+int b = int.Parse(Console.ReadLine());
+
+int max=a;
+int min=a;
+
+if (max > b)
+{
+    min= b;
+}
+else
+{
+   max = b;
+   min = a;   
+}
+
+Console.WriteLine($"max = {max}, min = {min}");
