1c578f566d5e92c461be0adc0e6f1031b3ab3ef8
tati <ost11tan@gmail.com>
2022-08-23 05:32:52 +0300

    task2,на заданных в тз значениях работает

---
+++ lesson1/task2/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+
+Console.Write("Введите число:");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число:");
+int b = int.Parse(Console.ReadLine());
+
+int max=a;
+int min=a;
+
+if (max > b)
+{
+    min= b;
+}
+else
+{
+   max = b;
+   min = a;   
+}
+
+Console.WriteLine($"max = {max}, min = {min}");
