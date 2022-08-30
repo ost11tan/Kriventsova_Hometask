f0b8a46ac411819f2e6b0111fca7b7179510f490
tati <ost11tan@gmail.com>
2022-08-31 02:31:08 +0300

    заданиe 25 double,чтоб не было переполнения

---
+++ lesson4/task25/Program.cs
@@ -0,0 +1,35 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+//3, 5 -> 243 (3⁵)
+
+//2, 4 -> 16
+
+
+
+
+
+
+double Pow(int number, int pow)
+{
+    double result = 1;
+    for (int i = 1; i <= pow; i++)
+    {
+        result*=number;
+    }
+    return result;
+}
+
+
+
+Console.WriteLine("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B: ");
+int B = int.Parse(Console.ReadLine());
+
+if (B<0)
+{
+    Console.WriteLine($"Нужно извлечь корень,без библиотеки сделать это не могу");
+}
+else
+    Console.WriteLine($"{Pow(A,B)}");
