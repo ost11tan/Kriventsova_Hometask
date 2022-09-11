8c63e94b6ee45a349dc11972a9cb23d9cc85f24e
tati <ost11tan@gmail.com>
2022-09-11 17:13:46 +0300

    задача 47

--- lesson7/task47/Program.cs
+++ lesson7/task47/Program.cs
@@ -39,7 +39,7 @@ void PrintArray(double[,] array)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            Console.Write($"{array[i, j]}\t");
+            Console.Write($"{array[i, j]:f2}\t");
         }
         Console.WriteLine();
     }
