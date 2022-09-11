c10d345a806670ff982073cac03e83bdb9f4bfc5
tati <ost11tan@gmail.com>
2022-09-11 17:10:48 +0300

    задача 47

---
+++ lesson7/task47/Program.cs
@@ -0,0 +1,59 @@
+﻿//Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+//m = 3, n = 4.
+
+//0,5 7 -2 -0,2
+
+//1 -3,3 8 -9,9
+
+//8 7,8 -7,1 9
+
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+
+    if (minValue > maxValue)
+    {
+        int temp = minValue;
+        minValue = maxValue;
+        maxValue = temp;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue);
+            result[i, j] = result[i, j] / 100;
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите два любых целых числа: ");
+int min = int.Parse(Console.ReadLine());
+int max = int.Parse(Console.ReadLine());
+
+double[,] myArray = GetArray(m, n, min, max);
+PrintArray(myArray);
