6cdd895e99d33c7e5f5fb894c8d9c5be08766d4d
tati <ost11tan@gmail.com>
2022-09-14 21:57:52 +0300

    задача 54

---
+++ lesson8/task54/Program.cs
@@ -0,0 +1,84 @@
+﻿//Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//В итоге получается вот такой массив:
+//7 4 2 1
+//9 5 3 2
+//8 4 4 2
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    if (minValue > maxValue)
+    {
+        int temp = minValue;
+        minValue = maxValue;
+        maxValue = temp;
+    }
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue+1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] SortArray(int[,] array)
+{
+    int row = array.GetLength(0);
+    int column = array.GetLength(1);
+    for (int i = 0; i < row; i++)
+    {
+        for (int k = column - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int temp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = temp;
+                }
+            }
+        }
+    }
+    return array;
+}
+
+Console.WriteLine("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов:");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите ддиапазон: ");
+int min = int.Parse(Console.ReadLine());
+int max = int.Parse(Console.ReadLine());
+
+int[,] myArray = GetArray(m, n, min, max);
+PrintArray(myArray);
+Console.WriteLine();
+
+int[,] mySortArray = SortArray(myArray);
+PrintArray(mySortArray);
